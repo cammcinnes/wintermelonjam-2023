553c0c56435163c1134794bc300770a700e30f29
Cam McInnes <camtmcinnes@gmail.com>
2023-12-28 13:50:51 -0800

    discard shader idea

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,9 @@
 extends Node
 
-var shader_material: ShaderMaterial
-var v = Vector2(0, 0)
 
 func newGame():
 	$player.start($start_position.position)
-	disappear(v)
+
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func disappear(vector2):
-	$TileMap.material.set_shader_parameter("fade_speed", 0.1)
-	$TileMap.material.set_shader_parameter("exclude_tile_coordinates", vector2)
