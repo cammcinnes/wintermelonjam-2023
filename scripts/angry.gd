985c5c2886cdbb0caca8033d9b92813a3886f22d
Cam McInnes <camtmcinnes@gmail.com>
2023-12-28 15:04:21 -0800

    Add homing pig

--- scripts/angry.gd
+++ scripts/angry.gd
@@ -1,9 +1,22 @@
 extends CharacterBody2D
+var speed = 100;
+var player_chase = false;
+var player
 
+func _physics_process(delta):
+	if player_chase:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * speed
+		move_and_collide(velocity * delta)
+		$AnimatedSprite2D.play("walk")
+	else: 
+		$AnimatedSprite2D.play("idle")
+		
 
 func _on_detection_area_body_entered(body):
-	pass # Replace with function body.
-
+	player = body
+	player_chase = true
 
 func _on_detection_area_body_exited(body):
-	pass # Replace with function body.
+	player = null
+	player_chase = false;
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,7 @@ extends Node
 
 func newGame():
 	$player.start($start_position.position)
-
+	$angry.position = $bad_position.position
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
