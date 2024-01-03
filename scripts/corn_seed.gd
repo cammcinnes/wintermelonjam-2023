fc5df1e9e62f99e8e0a9f17f4eb9cf459980edf3
Cam McInnes <camtmcinnes@gmail.com>
2024-01-03 11:33:06 -0800

    add comments to wheat_seed and corn_seed

--- scripts/corn_seed.gd
+++ scripts/corn_seed.gd
@@ -3,28 +3,30 @@ var selected = false
 var seed_type = 1 #corn
 var planted = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$AnimatedSprite2D.play("default")
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
 
 
+# select with mouse cursor
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("click"):
 		Main.plantselected = seed_type
 		selected = true
 
+# select with a left click
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and not event.pressed:
 			selected = false
 		
 
-
+# add plant to grow zone if not occupied
 func _on_area_2d_body_entered(body):
 	if body.has_method("can_grow") and not body.occupied:
 		selected = false
--- scripts/wheat_seed.gd
+++ scripts/wheat_seed.gd
@@ -4,27 +4,29 @@ var selected = false;
 var seed_type = 2 #wheat
 var planted = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$AnimatedSprite2D.play("default")
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
 
 
+# select wheat seed on click
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("click"):
 		Main.plantselected = seed_type
 		selected = true
 
+# only allow clicks with left mouse button
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and not event.pressed:
 			selected = false
 	
-
+# de select wheat seed if body is not occupied and grow zone
 func _on_area_2d_entered(body):
 	if body.has_method("can_grow") and not body.occupied:
 			selected = false
